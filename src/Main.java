b153705a2e1685189f96e803d2af778e1e8b3dc1
Ainur <ainurforex@mail.ru>
2022-06-20 17:18:10 +0500

    Lesson-6-HW

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,51 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        int[] arr = generateRandomArray();
-        String arrToString= Arrays.toString(arr);
-        System.out.println(arrToString);
+        homeWork1();
+        homeWork2();
+        homeWork3();
+        homeWork4();
     }
 
+    private static void homeWork1() {
+        int[] arr = generateRandomArray();
+        int summ=0;
+        for (int element : arr) {
+            summ+=element;
+        }
+        System.out.println("Сумма трат за месяц составила "+summ+ " рублей");
+    }
+
+    private static void homeWork2() {
+        int[] arr = generateRandomArray();
+        int min=arr[0];
+        int max=arr[0];
+        for (int element : arr) {
+            if (element<min)min=element;
+            if (element>max)max=element;
+        }
+        System.out.println("Минимальная сумма трат за день составила "+min+" рублей. "+
+                "Максимальная сумма трат за день составила "+max+" рублей.");
+
+    }
+    private static void homeWork3() {
+        int[] arr = generateRandomArray();
+        double avarage=0;
+        int summ=0;
+        for (int element : arr) {
+            summ+=element;
+        }
+        avarage=(double)summ/30;
+        System.out.println("Средняя сумма трат за месяц составила "+avarage+" рублей.");
+    }
+
+    private static void homeWork4() {
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i=reverseFullName.length-1; i>=0;i--) {
+            System.out.print(reverseFullName[i]);
+        }
+    }
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
