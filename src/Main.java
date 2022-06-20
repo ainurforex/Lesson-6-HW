4944e2e6b2b28e5134cf73c0e2a895af80e414b5
Ainur <ainurforex@mail.ru>
2022-06-20 16:42:16 +0500

    prepare

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int[] arr = generateRandomArray();
+        String arrToString= Arrays.toString(arr);
+        System.out.println(arrToString);
+    }
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
